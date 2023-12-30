4b9765e50c0686dadfc89bf41bbb7f0b0da7b4e2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-30 16:06:02 +0900

    feat(commit): implement command-line interface

--- packages/commit/src/index.ts
+++ packages/commit/src/index.ts
@@ -1,3 +1,26 @@
-const main = async () => {}
+import { program } from "commander"
+import { commit } from "./commands/commit"
+import { config } from "./commands/config"
+import { help } from "./commands/help"
+import { hook } from "./commands/hook"
 
-main()
+export const run = async () => {
+  program
+    .option("-g, --generate <generate>", ``)
+    .option("-a, --all", ``)
+    .action(commit)
+
+  program
+    .command("hook")
+    .addCommand(program.command("install", ``).action(hook.install))
+    .addCommand(program.command("uninstall", ``).action(hook.uninstall))
+
+  program
+    .command("config")
+    .addCommand(program.command("get [keys...]", ``).action(config.get))
+    .addCommand(program.command("set [parameters...]", ``).action(config.set))
+
+  program.on("-h, --help", help)
+
+  program.parse()
+}
