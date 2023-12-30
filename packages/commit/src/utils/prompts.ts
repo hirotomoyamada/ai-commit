b5f6bf310352c2bf4f0afd3135c69530d7a18b8d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-30 17:05:58 +0900

    refactor(utils): updated callback return type and outro

--- packages/commit/src/utils/prompts.ts
+++ packages/commit/src/utils/prompts.ts
@@ -1,21 +1,23 @@
 import * as p from "@clack/prompts"
 import c from "chalk"
 
+export type PromptOptions = { label: string; value: string }[]
+
 export type Prompts = typeof p
 
 export type Spinner = ReturnType<typeof p.spinner>
 
 export const prompts = async (
-  callback: (p: Prompts, s: Spinner) => Promise<void> | void,
+  callback: (p: Prompts, s: Spinner) => Promise<string | void> | string | void,
 ) => {
   p.intro(c.green(`AI Commit`))
 
   const s = p.spinner()
 
   try {
-    await callback(p, s)
+    const message = await callback(p, s)
 
-    p.outro(c.green(`Done\n`))
+    p.outro(c.green(`${message ?? "Done"}\n`))
   } catch (e) {
     s.stop(`An error occurred`, 500)
 
