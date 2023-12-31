31745d7820bece16a609728da68f37489aea64b0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-31 18:07:27 +0900

    refactor: update imports and improve CLI options

--- packages/commit/src/index.ts
+++ packages/commit/src/index.ts
@@ -1,28 +1,50 @@
 import { program } from "commander"
-import { commit } from "./commands/commit"
-import { config } from "./commands/config"
-import { help } from "./commands/help"
-import { hook } from "./commands/hook"
+import packageJSON from "../package.json"
+import commitAction from "./commands/commit"
+import configAction from "./commands/config"
+import helpAction from "./commands/help"
+// import hookAction from "./commands/hook"
 
 export const run = async () => {
-  program
-    .option("-g, --generate [generate]", ``)
-    .option("-a, --all", ``)
-    .option("-e, --excludes [files...]", ``)
-    .action(commit)
+  program.version(packageJSON.version, "-v, --version")
 
   program
-    .command("hook")
-    .addCommand(program.command("install", ``).action(hook.install))
-    .addCommand(program.command("uninstall", ``).action(hook.uninstall))
+    .option("-g, --generate [generate]", `Number of messages to generate`)
+    .option(
+      "-a, --all",
+      `Automatically stage tracked and untracked file changes for the commit`,
+    )
+    .option("-t, --type <type>", `Type of commit message to generate`)
+    .option("-e, --excludes [files...]", `Files to exclude from AI analysis`)
+    .action(commitAction)
 
-  program
-    .command("config")
-    .addCommand(program.command("get [keys...]", ``).action(config.get))
-    .addCommand(program.command("set [parameters...]", ``).action(config.set))
-    .addCommand(program.command("reset", ``).action(config.reset))
+  // const hook = program.command("hook")
 
-  program.on("-h, --help", help)
+  // hook
+  //   .command("install", "", { isDefault: true })
+  //   .description("Install git hooks")
+  //   .action(hookAction.install)
+  // hook
+  //   .command("uninstall")
+  //   .description(`Uninstall git hooks`)
+  //   .action(hookAction.uninstall)
+
+  const config = program.command("config")
+
+  config
+    .command("get [keys...]", "", { isDefault: true })
+    .description(`Get the configure`)
+    .action(configAction.get)
+  config
+    .command("reset [keys...]")
+    .description(`Reset the configure`)
+    .action(configAction.reset)
+  config
+    .command("set [parameters...]")
+    .description(`Set the configure`)
+    .action(configAction.set)
+
+  program.on("-h, --help", helpAction)
 
   program.parse()
 }
