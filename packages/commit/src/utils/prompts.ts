fcef2a85bd883a5ff01b62fc21c4262f9f849899
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-31 15:56:44 +0900

    feat(utils): added error handling to prompts

--- packages/commit/src/utils/prompts.ts
+++ packages/commit/src/utils/prompts.ts
@@ -6,6 +6,7 @@ export type Option = { value: string; label?: string; hint?: string }
 type CustomCommands = {
   done: (message: string) => void
   complete: (message: string) => void
+  error: (message: string) => void
 }
 
 export type Prompts = typeof p & CustomCommands
@@ -19,17 +20,33 @@ export const prompts = async (
 
   p.intro(c.green(`AI Commit`))
 
+  let isLoading = false
   const s = p.spinner()
 
   try {
     const commands: CustomCommands = {
       done: (message) => p.outro(c.dim(message)),
       complete: (message) => p.outro(c.green(message)),
+      error: (message) => p.cancel(c.red(message)),
     }
 
-    await callback({ ...p, ...commands }, s)
+    const spinner: Spinner = {
+      start: (message) => {
+        isLoading = true
+
+        s.start(message)
+      },
+      message: (message) => s.message(message),
+      stop: (message) => {
+        isLoading = false
+
+        s.stop(message)
+      },
+    }
+
+    await callback({ ...p, ...commands }, spinner)
   } catch (e) {
-    s.stop(`An error occurred`, 500)
+    if (isLoading) s.stop(`An error occurred`, 500)
 
     p.cancel(c.red(e instanceof Error ? e.message : "Message is missing"))
   }
