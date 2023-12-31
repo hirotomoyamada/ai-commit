7725f581bf9606a765bc03c85042afc43f5a4e99
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-31 12:32:13 +0900

    feat(utils): add custom commands to prompts object

--- packages/commit/src/utils/prompts.ts
+++ packages/commit/src/utils/prompts.ts
@@ -3,22 +3,34 @@ import c from "chalk"
 
 export type PromptOptions = { label: string; value: string }[]
 
-export type Prompts = typeof p
+type CustomCommands = {
+  done: (message: string) => void
+  complete: (message: string) => void
+}
+
+export type Prompts = typeof p & CustomCommands
 
 export type Spinner = ReturnType<typeof p.spinner>
 
 export const prompts = async (
-  callback: (p: Prompts, s: Spinner) => Promise<string | void> | string | void,
+  callback: (p: Prompts, s: Spinner) => Promise<void> | void,
 ) => {
+  console.log("")
+
   p.intro(c.green(`AI Commit`))
 
   const s = p.spinner()
 
   try {
-    const message = await callback(p, s)
+    const commands: CustomCommands = {
+      done: (message) => p.outro(c.dim(message)),
+      complete: (message) => p.outro(c.green(message)),
+    }
 
-    p.outro(c.green(`${message ?? "Done"}\n`))
+    await callback({ ...p, ...commands }, s)
   } catch (e) {
+    console.log(e)
+
     s.stop(`An error occurred`, 500)
 
     p.cancel(c.red(e instanceof Error ? e.message : "Message is missing"))
