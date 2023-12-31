956a27e95e07c48f797019864cc0e2f310e76446
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-31 17:04:14 +0900

    feat(commit): add new file for commit functionality

---
+++ packages/commit/src/commands/commit/index.ts
@@ -0,0 +1,143 @@
+import c from "chalk"
+import { execa } from "execa"
+import type { Config } from "../../utils/config"
+import { getConfig, onValidateProperty } from "../../utils/config"
+import {
+  getDiff,
+  getNotStagedFiles,
+  getStagedFiles,
+  getUntrackedFiles,
+} from "../../utils/git"
+import { generateCommitMessages } from "../../utils/openai"
+import type { Option, Prompts, Spinner } from "../../utils/prompts"
+import { prompts } from "../../utils/prompts"
+
+let config: Config
+
+const detectStagedFiles =
+  (all: boolean, excludeFiles: string[]) => async (p: Prompts, s: Spinner) => {
+    s.start("Detecting the staged files")
+
+    if (all) await execa("git", ["add", "."])
+
+    const { files, diff } = await getStagedFiles(excludeFiles)
+
+    s.stop("Detected the staged files")
+
+    if (files?.length) {
+      const count = files.length.toString()
+      const list = files.join("\n")
+
+      p.note(list, `Staged ${count} file${files.length > 1 ? "s" : ""}`)
+
+      return diff
+    } else {
+      s.start("Detecting the worked files")
+
+      const notStagedFiles = await getNotStagedFiles(excludeFiles)
+      const untrackedFiles = await getUntrackedFiles(excludeFiles)
+
+      s.stop("Detecting the worked files")
+
+      if (!notStagedFiles.length && !untrackedFiles.length) {
+        p.done("No worked files found")
+
+        return
+      }
+
+      const options: Option[] = [
+        ...notStagedFiles.map((value) => ({ value })),
+        ...untrackedFiles.map((value) => ({ value, hint: "Untracked" })),
+      ].sort((a, b) => a.value.localeCompare(b.value))
+
+      const selectedFiles = await p.groupMultiselect<Option[], string>({
+        message: `Select files to commit: ${c.dim("(Ctrl+c to exit)")}`,
+        options: { "Select all": options },
+      })
+
+      if (p.isCancel(selectedFiles)) {
+        p.done("Commit cancelled")
+
+        return
+      }
+
+      await execa("git", ["add", ...selectedFiles])
+
+      const diff = await getDiff(excludeFiles)
+
+      return diff
+    }
+  }
+
+const commitMessage =
+  (generate: string, diff: string) => async (p: Prompts, s: Spinner) => {
+    s.start("Analyzing your changes with the AI")
+
+    const messages = await generateCommitMessages({ ...config, diff, generate })
+
+    let message: string
+
+    if (!messages.length)
+      throw new Error("No commit messages were generated. Try again.")
+
+    s.stop("Analyzed your changes")
+
+    if (messages.length === 1) {
+      message = messages[0]
+
+      p.note(message, "Commit message")
+
+      const shouldContinue = await p.confirm({
+        message: `Use this commit message?`,
+      })
+
+      if (p.isCancel(shouldContinue) || !shouldContinue) {
+        p.done("Reset cancelled")
+
+        return
+      }
+    } else {
+      const selectedMessage = await p.select<Option[], string>({
+        message: `Pick a commit message to use: ${c.dim("(Ctrl+c to exit)")}`,
+        options: messages.map((value) => ({ value })),
+      })
+
+      if (p.isCancel(selectedMessage)) {
+        p.done("Commit cancelled")
+
+        return
+      }
+
+      message = selectedMessage
+    }
+
+    await execa("git", ["commit", "-m", message])
+
+    p.complete("Successfully committed")
+  }
+
+type Options = {
+  generate: string
+  all: boolean
+  excludes: string[]
+}
+
+export const commit = async ({
+  generate = "1",
+  all = false,
+  excludes = [],
+}: Options) => {
+  await prompts(async (p, s) => {
+    config = await getConfig()
+
+    onValidateProperty(
+      "apiKey",
+      !!config.apiKey,
+      "Please set your OpenAI API key via `ai-commit config set apiKey=<your token>`",
+    )
+
+    const diff = await detectStagedFiles(all, excludes)(p, s)
+
+    if (diff) await commitMessage(generate, diff)(p, s)
+  })
+}
