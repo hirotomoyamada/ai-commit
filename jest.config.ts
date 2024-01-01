f47b9ff6af9ba009161c66cfd50b98a1dbaa88aa
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-01-02 06:03:10 +0900

    refactor: removed .turbo and restructured project

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "jest"
 
 const config: Config = {
-  collectCoverageFrom: ["packages/**/*.{ts}"],
+  collectCoverageFrom: ["tests/**/*.{ts}"],
   moduleFileExtensions: ["ts", "js"],
   transform: {
     "^.+\\.(ts|js)?$": ["@swc-node/jest", { module: "commonjs" }],
